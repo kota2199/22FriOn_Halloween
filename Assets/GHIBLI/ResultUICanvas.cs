bb69d5807d4b011971171af54b499fc016325f51
GHIBLI <yamaoka1013hp@gmail.com>
2023-10-27 05:52:18 +0900

    GameOverかタイムアップかの表記と、フレーバーテキスト
    フレーバーテキストは4種類

--- Assets/GHIBLI/ResultUICanvas.cs
+++ Assets/GHIBLI/ResultUICanvas.cs
@@ -6,21 +6,23 @@ using UnityEngine.UI;
 public class ResultUICanvas : MonoBehaviour
 {
     enum YokaiName{};
-    string[] yokaiNames = { "ひとつめ小僧", "かっぱ", "ぬらりひょん", "座敷わらし", "提灯お化け", "ろくろ首", "アマビエ", "天狗" };
+    string[] yokaiNames = { "一つ目小僧", "かっぱ", "ぬらりひょん", "座敷わらし", "提灯お化け", "ろくろ首", "アマビエ", "天狗" };
 
     [SerializeField] Sprite[] yokaiSprites;
 
+    [SerializeField] Text resultText;
     [SerializeField] Image resultYokaiImage;
     [SerializeField] Text resultScoreText;
     [SerializeField] Text resultYokaiNameText;
 
     [SerializeField] Text flavourText;
 
-    string[] flavourtexts =
+    string[] flavourTexts =
     {
         "お疲れさま！！",
         "よく頑張ったね！",
-        "わっしょい！",
+        "次はもっと上を目指そう！",
+        "また遊んでね",
     };
 
     // Start is called before the first frame update
@@ -37,7 +39,12 @@ public class ResultUICanvas : MonoBehaviour
         {
             int score = Random.Range(0, 1000)*10;
             int yokaiIndex = Random.Range(0, 8);
-            SetActiveResultUICanvas(!gameObject.GetComponent<Canvas>().enabled, score, yokaiIndex);
+            bool result;
+            if (Random.Range(0f, 1f) < 0.5f)
+                result = true;
+            else
+                result = false;
+            SetActiveResultUICanvas(!gameObject.GetComponent<Canvas>().enabled, score, yokaiIndex, result);
         }
     }
 
@@ -45,16 +52,20 @@ public class ResultUICanvas : MonoBehaviour
     //引数1 value：UI(Canvas)のCanvasコンポーネントのSetActive(value)
     //引数2 score：そのままスコアになります
     //引数3 yokaiIndex：0～7で指定してください. 妖怪の画像、妖怪名を指定するのに配列のindexとして使います
-    ////0が一つ目小僧で、7が天狗です
-    public void SetActiveResultUICanvas(bool value,int score,int yokaiIndex)
+    //0が一つ目小僧で、7が天狗です
+    //引数4 isTimeUp：タイムアップで終了したかどうか、falseならゲームオーバーとして終了
+    public void SetActiveResultUICanvas(bool value,int score,int yokaiIndex, bool isTimeUp)
     {
         gameObject.GetComponent<Canvas>().enabled = value;
+
+        resultText.text = isTimeUp ? "タイムアップ！" : "ゲームオーバー！";
+        
 
         resultYokaiImage.sprite = yokaiSprites[yokaiIndex];
         resultScoreText.text = score.ToString();
 
         resultYokaiNameText.text = yokaiNames[yokaiIndex];
 
-        flavourText.text = flavourtexts[Random.Range(0, flavourtexts.Length)];
+        flavourText.text = flavourTexts[Random.Range(0, flavourTexts.Length)];
     }
 }
