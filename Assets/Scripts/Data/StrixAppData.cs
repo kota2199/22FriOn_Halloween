e45ff59f80f8dbf1b1886735725722ef6b16ffb3
結城康太 <kota.2199@icloud.com>
2024-08-03 02:06:10 +0900

    no message

--- Assets/Scripts/Data/StrixAppData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "StrixAppData", menuName = "CreateData/StrixAppData")]
-public class StrixAppData : ScriptableObject
-{
-    public string applicationID, hostUrl;
-}
